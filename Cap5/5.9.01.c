10c282bde361addeeff604e7416a64a42ab9f86d
Matheus <matheusrod3@gmail.com>
2024-01-24 23:24:01 -0300

    Update 5.9.01.c

--- Cap5/5.9.01.c
+++ Cap5/5.9.01.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
@@ -55,3 +56,62 @@ int main(){
 }
 
 
+=======
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+//for
+int main(){
+    int num, cont;
+    setlocale(LC_ALL,"");
+    printf("Digite um número natural: ");
+    scanf("%d",&num);
+    for (cont = 0; cont <= num;cont++){
+        printf("%d ",cont);
+    }
+    return 0;
+}
+
+//while
+int main(){
+    int num, cont;
+    setlocale(LC_ALL,"");
+    printf("Digite um número natural: ");
+    scanf("%d",&num);
+    while (cont <= num){
+        printf("%d ",cont);
+        cont++;
+    }
+    return 0;
+}
+
+//do-while
+int main(){
+    int num, cont;
+    setlocale(LC_ALL,"");
+    printf("Digite um número natural: ");
+    scanf("%d",&num);
+    do{
+        printf("%d ",cont);
+        cont++;
+    } while(cont<=num);
+    return 0;
+}
+
+//goto
+int main(){
+    int num, cont;
+    setlocale(LC_ALL,"");
+    printf("Digite um número natural: ");
+    scanf("%d",&num);
+    sequencia:
+        if (cont <= num){
+            printf("%d ",cont);
+            cont++;
+            goto sequencia;
+        }
+    return 0;
+}
+
+
+>>>>>>> 7bc82ce439215a72f80c44345bbe8dc0aaefce6c
